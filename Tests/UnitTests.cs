ddc89ad9618b4b5016c8ec97f434e7b9c619c6c4
Liam Makela <liammakela06@gmail.com>
2026-03-25 23:36:52 -0500

    added unit tests for all operations

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -26,4 +26,35 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
+	[TestMethod]
+	public void TestAdd()
+	{
+		Assert.AreEqual(999, Program.Add("2", "3"));
+		Assert.AreEqual(0, Program.Add("-2", "2"));
+	}
+	[TestMethod]
+	public void TestSubtract()
+	{
+		Assert.AreEqual(1, Program.Subtract("3", "2"));
+		Assert.AreEqual(-4, Program.Subtract("-2", "2"));
+	}
+	[TestMethod]
+	public void TestMultiply()
+	{
+		Assert.AreEqual(6, Program.Multiply("2", "3"));
+		Assert.AreEqual(0, Program.Multiply("0", "5"));
+	}
+	[TestMethod]
+	public void TestDivide()
+	{
+		Assert.AreEqual(2, Program.Divide("6", "3"));
+		Assert.AreEqual(2.5, Program.Divide("5", "2"));
+	}
+	[TestMethod]
+	public void TestPower()
+	{
+		Assert.AreEqual(8, Program.Power("2", "3"));
+		Assert.AreEqual(1, Program.Power("5", "0"));
+		Assert.AreEqual(9, Program.Power("3", "2"));
+	}
 }
